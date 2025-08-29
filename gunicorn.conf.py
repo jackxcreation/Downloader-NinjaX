d44b8dc19d10cd109164a6f1e33adb72622a38a4
jackxcreation <jodjack64@gmail.com>
2025-08-29 20:34:14 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,21 +1,23 @@
-# Gunicorn configuration file
 import os
+import multiprocessing
 
 # Server socket
-bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
+bind = f"0.0.0.0:{os.environ.get('PORT', 10000)}"
 backlog = 2048
 
 # Worker processes
-workers = 4
-worker_class = "sync"
+workers = min(multiprocessing.cpu_count() * 2 + 1, 8)
+worker_class = "gthread"
+threads = 2
 worker_connections = 1000
-timeout = 300
-keepalive = 30
 max_requests = 1000
-max_requests_jitter = 100
-
-# Restart workers after this many requests, to help prevent memory leaks
+max_requests_jitter = 50
 preload_app = True
+
+# Restart workers after this many seconds
+timeout = 120
+keepalive = 2
+graceful_timeout = 30
 
 # Logging
 accesslog = "-"
@@ -24,20 +26,33 @@ loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # Process naming
-proc_name = 'downloader-ninjax'
+proc_name = "downloader-ninjax"
 
 # Server mechanics
 daemon = False
-pidfile = '/tmp/gunicorn.pid'
+pidfile = "/tmp/gunicorn.pid"
 user = None
 group = None
 tmp_upload_dir = None
 
-# SSL
-keyfile = None
-certfile = None
+# SSL (if certificates are available)
+# keyfile = "/etc/ssl/private/server.key"
+# certfile = "/etc/ssl/certs/server.crt"
 
-# Security
-limit_request_line = 4096
-limit_request_fields = 100
-limit_request_field_size = 8190
+def when_ready(server):
+    server.log.info("Downloader NinjaX API is ready to serve requests")
+
+def worker_int(worker):
+    worker.log.info("Worker received INT or QUIT signal")
+
+def pre_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+def post_fork(server, worker):
+    server.log.info("Worker spawned (pid: %s)", worker.pid)
+
+def post_worker_init(worker):
+    worker.log.info("Worker initialized")
+
+def worker_abort(worker):
+    worker.log.info("Worker received SIGABRT signal")
